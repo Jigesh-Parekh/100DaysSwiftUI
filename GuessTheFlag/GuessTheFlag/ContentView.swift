69639637228cf5fdfd1536eb597ad929360e387a
Jigesh Parekh <JParekh117@gmail.com>
2020-05-06 16:30:39 -0700

    Update Guess the Flag to animate inputs

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,6 +8,19 @@
 
 import SwiftUI
 
+
+struct FlagImage: View {
+    var image: String
+    
+    var body: some View {
+        Image(image)
+        .renderingMode(.original)
+        .clipShape(Capsule())
+        .overlay(Capsule().stroke(Color.black, lineWidth: 1))
+        .shadow(color: .black, radius: 9)
+    }
+}
+
 struct ContentView: View {
     
     @State private var countries  = ["estonia", "france", "germany", "ireland",
@@ -19,20 +32,25 @@ struct ContentView: View {
     @State private var scoreTitle = ""
     @State private var pointConnter = 0
     
+    @State private var opacityAmount = 1.0
+    @State private var rotationAmount = 0.0
+    @State private var wrongRotationAmount = [0.0, 0.0, 0.0]
+    
     
     var body: some View {
         ZStack {
             
             LinearGradient(gradient: Gradient(colors: [.blue, .black]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
             
-        
-            
-            //Color.blue.edgesIgnoringSafeArea(.all)
             VStack(spacing: 40) {
                 
                 VStack{
                     Text("Tap the flag of").foregroundColor(.white)
-                    Text(countries[correctAnswer]).foregroundColor(.white).font(.largeTitle).fontWeight(.bold)
+                    
+                    Text(countries[correctAnswer])
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
                 }
                 
              
@@ -40,15 +58,14 @@ struct ContentView: View {
                 ForEach(0 ..< 3) {  number in
                     Button(action: {
                         //flag was tapped
+                        self.opacityAmount = 0.2
                         self.flagTapped(number)
                     }) {
-                        Image(self.countries[number])
-                            .renderingMode(.original).clipShape(Capsule())
-                                
-                                .overlay(Capsule().stroke(Color.black, lineWidth: 1))
-                        
-                            .shadow(color: .black, radius: 9)
+                        FlagImage(image: self.countries[number])
                     }
+                        .opacity(number == self.correctAnswer ? 1 : self.opacityAmount)
+                        .rotation3DEffect(.degrees(number == self.correctAnswer ? self.rotationAmount : 0),axis: (x: 0, y: 1, z: 0))
+                        .rotation3DEffect(.degrees(self.wrongRotationAmount[number]),axis: (x: 0, y: 1, z: 0))
                     
                 }
                 
@@ -68,9 +85,17 @@ struct ContentView: View {
         if number == correctAnswer {
             scoreTitle = "Correct"
             pointConnter += 1
+            
+            withAnimation(.interpolatingSpring(stiffness: 20, damping: 5)) {
+                self.rotationAmount = 360
+            }
         } else {
             scoreTitle = "Wrong Flag, that is the flag of \(countries[number])"
             pointConnter -= 1
+            
+            withAnimation(Animation.interpolatingSpring(mass: 1, stiffness: 120, damping: 40, initialVelocity: 200)) {
+                self.wrongRotationAmount[number] = 5
+            }
         }
         
         showingScore = true
@@ -79,6 +104,12 @@ struct ContentView: View {
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
+        withAnimation(.easeInOut) {
+            self.opacityAmount = 1.0
+        }
+        self.rotationAmount = 0.0
+        wrongRotationAmount = Array(repeating: 0.0, count: 3)
+        
     }
 }
 
