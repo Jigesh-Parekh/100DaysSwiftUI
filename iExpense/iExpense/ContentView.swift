4d5921473d7e212414b1290ed257cc818fc48a2f
Jigesh Parekh <JParekh117@gmail.com>
2020-05-13 17:14:09 -0700

    reset project to inital state

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -8,56 +8,12 @@
 
 import SwiftUI
 
-
-//global classes
-
-struct SecondView: View {
-    @Environment(\.presentationMode) var presentationMode
-    var body: some View {
-        Button("Dismiss") {
-            self.presentationMode.wrappedValue.dismiss()
-        }
-    }
-}
-
-//local struct
-
-
 struct ContentView: View {
-    @State private var showSheet = false
-    
-    @State private var numbers = [Int]()
-    @State private var currentNum  = 1
-    @State private var tapCount  = UserDefaults.standard.integer(forKey: "Tap")
+
     var body: some View {
-        NavigationView{
-            VStack {
-                List {
-                    Text("TapCount: \(self.tapCount)")
-                    ForEach(numbers, id: \.self) {
-                        Text("\($0)")
-                    }
-                    .onDelete(perform: removeRows)
-                }
-                Button("Add Numbers") {
-                    self.numbers.append(self.currentNum)
-                    self.currentNum += 1
-                }
-                Button("TapCount") {
-                    self.tapCount += 1
-                    UserDefaults.standard.set(self.tapCount, forKey: "Tap")
-                }
-            }
-            .navigationBarItems(leading: EditButton())
-        }
-        
+       Text("HelloWorld")
     }
-
-    func removeRows(at offsets: IndexSet) {
-          numbers.remove(atOffsets: offsets)
-      }
 }
-
 
 
 struct ContentView_Previews: PreviewProvider {
