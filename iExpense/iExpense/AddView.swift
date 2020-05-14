cf16ae3f021e01270c4decc4922d42e9027b8ba6
Jigesh Parekh <JParekh117@gmail.com>
2020-05-13 19:22:53 -0700

    Sharing data between views

---
+++ iExpense/iExpense/AddView.swift
@@ -0,0 +1,41 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Jigesh Parekh on 5/13/20.
+//  Copyright © 2020 Jigesh Parekh. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @ObservedObject var expenses: Expenses
+    
+    @State private var name = ""
+    @State private var type = "personal"
+    @State private var amount = ""
+    
+    static let types = ["Business","Personal"]
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", text: $amount).keyboardType(.numberPad)
+            }
+        .navigationBarTitle("Add new expense")
+        }
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
